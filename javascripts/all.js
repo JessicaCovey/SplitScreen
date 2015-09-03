aed9e8c8f63911a9fd960215c60d647ca7caefeb
Jessica Covey <coveyjessica@gmail.com>
2015-09-03 14:54:00 -0700

    Automated commit at 2015-09-03 21:54:00 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,6 +1,6 @@
 var chapters = document.querySelectorAll('[data-target]');
 
-window.addEventListener('scroll', function() {
+function updateImage() {
   var last = chapters[0];
   for(var i = 0; i < chapters.length; i++) {
     if (chapters[i].getBoundingClientRect().top < 0) {
@@ -19,7 +19,26 @@ window.addEventListener('scroll', function() {
   if (bad != null) {
     bad.classList.remove('shown');
   }
+}
+
+
+var timer;
+window.addEventListener('scroll', function() {
+  clearTimeout(timer);
+  timer = setTimeout(updateImage, 0);
 });
+
+//function myFunction() {
+    // Explanation 
+    // every time we scroll we set a setTimeout to one second, it will keep resetting itself to one second until the user 
+    // stops scrolling.  when they stop it will show the picture 
+
+    // create a variable that gets the id of the setTimeout(function() {}, 1000);
+    //  and use it to cancel the old one before we set a new one
+    //  this variable has to go outside of the addEventListener function
+
+
+
 // look at all the chapters and look which one has a negative get bounding client rect.top
 // 
 // grab the last negative image and show that one
